5025ff8a5425e94fa6883a007f5ce7757fa5fec0
DOUGLAS <145362569+Mirabel-Madrigal@users.noreply.github.com>
2025-05-20 11:38:15 -0300

    aula20/05

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const botoes = document.querySelectorAll (".botao");
+const textos = document.querySelectorAll (".aba-conteudo");
 
 for (let i = 0; i <botoes.leng; i++){
     botoes [i].onclick = function (){
+
         for (let j = 0; j <botoes.length; j++){
             botoes[j].classlist.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
     
-        botoes(i).classlist.add("ativo");
+        botoes[i].classlist.add("ativo");
+        textos[i].classList.add("ativo");
     };
 }
+
+const contadores = document.querySelectorAll ("contador");
+const tempoObjetivo1 = new Date ("2025-08-26t00:00:00");
